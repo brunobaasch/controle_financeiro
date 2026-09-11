5651c18969129ae546c5a2e4c001debdb9f0ba57
Bruno <brunobaasch1@gmail.com>
2026-09-11 18:30:40 -0300

    fix

--- controleFinanceiro/src/com/controleFinanceiro/model/Conta.java
+++ controleFinanceiro/src/com/controleFinanceiro/model/Conta.java
@@ -3,7 +3,45 @@ package com.controleFinanceiro.model;
 import java.math.BigDecimal;
 
 public class Conta {
+    private static int contador = 0;
     private int id;
     private String nome;
-    private BigDecimal saldo;
+    private BigDecimal saldo = new BigDecimal(0);
+
+    public Conta(String nome) {
+        this.id = ++contador;
+        this.nome = nome;
+    }
+
+    //metodos
+    public boolean debitar(String valor) {
+        if (valor.)
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
 }
--- controleFinanceiro/src/com/controleFinanceiro/model/Transacao.java
+++ controleFinanceiro/src/com/controleFinanceiro/model/Transacao.java
@@ -4,11 +4,18 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Transacao {
-    private int id;
-    private String contaAssociada;
+    private int id = 1;
     private String categoria;
     private BigDecimal valor;
-    private TipoTransacao tipo;
+//  private TipoTransacao tipo;
     private LocalDate data;
     private String descricao;
+
+    public Transacao(BigDecimal valor, String categoria, String descricao) {
+        this.valor = valor;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.id = this.id++;
+        this.data = LocalDate.now();
+    }
 }
