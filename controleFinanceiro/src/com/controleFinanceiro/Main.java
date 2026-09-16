1128bdbfee6fc2f44cf939ae02bbf1288f6ed4e4
Bruno <brunobaasch1@gmail.com>
2026-09-15 23:07:11 -0300

    try e catch

--- controleFinanceiro/src/com/controleFinanceiro/Main.java
+++ controleFinanceiro/src/com/controleFinanceiro/Main.java
@@ -1,11 +1,12 @@
 package com.controleFinanceiro;
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
 import java.util.*;
 import com.controleFinanceiro.model.*;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 1;
+        int x = -1;
         Scanner scanner = new Scanner(System.in);
         Conta c1 = new Conta("Bruno");
         c1.creditar(BigDecimal.valueOf(2000));
@@ -14,18 +15,28 @@ public class Main {
             System.out.println("1- Registrar gasto" +
                     "\n2- Ver gastos" +
                     "\n0- Sair");
-            x = scanner.nextInt();
-
+//            while (true) {
+                try {
+                    x = scanner.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Digite um número!");
+                    scanner.next();
+                }
+//            }
             switch (x) {
                 case 1 -> {
                     scanner.nextLine();
                     System.out.println("Digite o valor do gasto: ");
-                    String valor = scanner.nextLine();
-                    BigDecimal val = new BigDecimal(valor);
-                    System.out.println("Digite a categoria: ");
-                    String cat = scanner.nextLine();
-                    boolean teste = c1.registrarTransacao(val, cat);
-                    System.out.println(teste);
+                    try {
+                        String valor = scanner.nextLine();
+                        BigDecimal val = new BigDecimal(valor);
+                        System.out.println("Digite a categoria: ");
+                        String cat = scanner.nextLine();
+                        boolean teste = c1.registrarTransacao(val, cat);
+                        System.out.println(teste);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Digite um valor númerico!");
+                    }
                 }
                 case 2 -> {
                     List<Transacao> l = new ArrayList<>(c1.getTransacoes());
