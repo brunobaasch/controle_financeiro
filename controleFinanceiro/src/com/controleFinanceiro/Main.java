cc1536472345fc1d8694181d5a69384c53782c3b
Bruno <brunobaasch1@gmail.com>
2026-09-22 00:09:45 -0300

    função consulta main ok

--- controleFinanceiro/src/com/controleFinanceiro/Main.java
+++ controleFinanceiro/src/com/controleFinanceiro/Main.java
@@ -97,12 +97,10 @@ public class Main {
 
             BufferedReader reader = new BufferedReader(new FileReader("dadosTransacao.csv"));
             String linha = reader.readLine();
-            System.out.println("Li do arquivo:\n" + linha);
-            linha = reader.readLine();
             while (linha != null) {
                 String[] t = linha.split(";");
                 LocalDate data = LocalDate.parse(t[3]);
-                if (data.isAfter(ini) && data.isBefore(fim)) {
+                if (!data.isBefore(ini) && !data.isAfter(fim)) {
                     transacoes.add(linha);
                 }
                 linha = reader.readLine();
